819b85b56733350c530846a4893d6f71232c6581
William-io <capuletos@live.com>
2021-06-26 19:05:21 -0300

    Product List and Saving to Database.

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,6 +6,7 @@ namespace Shop.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string Type { get; set; }
         
     }
     
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Shop.Models;
+using Shop.Raven;
 
 namespace Shop
 {
@@ -6,9 +8,27 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("");
+            CreateProduct(name: "Dell", type: "Accessory", price: 9.10);
+            CreateProduct(name: "Mouse Logtech", type: "Accessory", price: 2.10);
         }
 
-        //
+        //List products
+        static void CreateProduct(string name, string type, double price)
+        {
+            Product products = new Product();
+            products.Name = name;
+            products.Type = type;
+            products.Price = price;
+
+            //save to database
+
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                //Access 
+                session.Store(products);
+                session.SaveChanges();
+            }
+        }
     }
 }
